e63a081270dfa6534156738ec817732a7af19d3f
Talon Bowler <nolat301@gmail.com>
2017-12-13 16:56:43 -0700

    Change from spawnfiller to spawnfill

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,12 @@ require('proto');
 
 var harvester = require('harvester');
 var upgrade_harvester = require('upgrade_harvester');
-var spawnfiller_harvester = require('spawnfiller_harvester');
+var spawnfill_harvester = require('spawnfill_harvester');
 
 var roles = {};
 roles[harvester.role] = harvester;
 roles[upgrade_harvester.role] = upgrade_harvester;
-roles[spawnfiller_harvester.role] = spawnfiller_harvester;
+roles[spawnfill_harvester.role] = spawnfill_harvester;
 
 function clean_memory() {
 	_.forEach (Memory.creeps, (creep, name) => {
@@ -28,14 +28,14 @@ module.exports.loop = function () {
 		let path = spawn1.room.controller.pos.findPathTo(source, {ignoreCreeps : true});
 		spawn1.room.visual.poly(path, {stroke : 'black'});
 	});
-	let path = spawn1.room.controller.pos.findPathTo(spawn1, {ignoreCreeps : true});
+	let path = spawn1.room.controller.pos.findPathTo(spawn1, {ignoreCreeps : true, ignoreRoads : true});
 	spawn1.room.visual.poly(path, {stroke : 'black'});
 	clean_memory();
 	_.forEach (Game.creeps, creep => {
 		if (creep.spawning) {
 			return;
 		}
-		creep.transition(creep.room, roles, spawnfiller_harvester);
-		creep.action(creep.room, roles, spawnfiller_harvester);
+		creep.transition(creep.room, roles, spawnfill_harvester);
+		creep.action(creep.room, roles, spawnfill_harvester);
 	});
 };
