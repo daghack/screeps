626f1ec8da283119e6db68a4131a53bb0ae0a9be
Talon Bowler <nolat301@gmail.com>
2017-12-12 19:46:20 -0700

    Add basic prototypes

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
+Creep.prototype.moveTowards = function(target) {
+	if this.getRangeTo(target) > 1 {
+		this.moveTo(target);
+	}
+};
+
+Source.prototype.slots = function() {
+	return this.room.adjacent_plains(this.pos).length;
+};
+
+Room.protototype.sources = function() {
+	return this.find(FIND_SOURCES);
+};
+
+Room.protototype.adjacent_plains = function(pos) {
+	let top_b = Math.max(0, pos.y-1)
+	let bottom_b = Math.min(49, pos.y+1)
+	let left_b = Math.max(0, pos.x-1)
+	let right_b = Math.min(49, pos.x+1)
+	let area = this.lookAtArea(top_b, left_b, bottom_b, right_b, true);
+	return _.filter(area, {type : 'terrain', terrain: 'plain'});
+};
+
 module.exports.loop = function () {
-}
+	for(let creep of Game.creeps) {
+	}
+	var sources = creep.room.find(FIND_SOURCES);
+	for(let source of sources) {
+		console.log(source.slots());
+	}
+	if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+		creep.moveTo(sources[0]);
+	}
+};
