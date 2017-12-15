bc0519790d7426fb6468f72333a2cd1df5dc675b
Talon Bowler <nolat301@gmail.com>
2017-12-15 13:26:07 -0700

    Only define property if it doesn't already exist

--- scripts/proto.js
+++ scripts/proto.js
@@ -3,25 +3,27 @@ global.HARVEST = 'harvest';
 global.UPGRADE = 'upgrade';
 global.SPAWNFILL = 'spawnfill';
 global.memory_property = function(obj, key, def, is_constructor) {
-	Object.defineProperty(obj, key, {
-		get : function() {
-			if (!this.memory[key]) {
-				if(is_constructor) {
-					// Because defineProperty only ever is called once, def will by default be a shared object.
-					// For constants this is fine, but if we want a list or object, we need to pass in the constructor.
-					// This will cause def to be the constructor so that each time we fetch,
-					// if we don't already have something at that key in the memory, we'll generate the new object.
-					this.memory[key] = new def();
-				} else {
-					this.memory[key] = def;
+	if (!obj[key]) {
+		Object.defineProperty(obj, key, {
+			get : function() {
+				if (!this.memory[key]) {
+					if(is_constructor) {
+						// Because defineProperty only ever is called once, def will by default be a shared object.
+						// For constants this is fine, but if we want a list or object, we need to pass in the constructor.
+						// This will cause def to be the constructor so that each time we fetch,
+						// if we don't already have something at that key in the memory, we'll generate the new object.
+						this.memory[key] = new def();
+					} else {
+						this.memory[key] = def;
+					}
 				}
+				return this.memory[key];
+			},
+			set : function(x) {
+				this.memory[key] = x;
 			}
-			return this.memory[key];
-		},
-		set : function(x) {
-			this.memory[key] = x;
-		}
-	});
+		});
+	}
 };
 
 global.add_memory = function(obj, tag, unique_id) {
