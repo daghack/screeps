08e26c22cc27e873c615d627b85740cf5bb3894c
Talon Bowler <nolat301@gmail.com>
2020-04-20 17:59:48 -0700

    Different object method

--- v2/overlord.js
+++ v2/overlord.js
@@ -11,7 +11,7 @@ function initialize_overlord() {
 	};
 };
 
-Overlord.prototype.tick = function() {
+Overlord.tick = function() {
 	console.log("Overlord Tick");
 	_.remove(Memory.overlord.registered_spawns, function(id) {
 		var spawn = Game.getObjectById(id);
@@ -23,27 +23,27 @@ Overlord.prototype.tick = function() {
 	});
 };
 
-Overlord.prototype.run_spawn = function(spawn) {
+Overlord.run_spawn = function(spawn) {
 };
 
-Overlord.prototype.is_spawn_registered = function(id) {
+Overlord.is_spawn_registered = function(id) {
 	return !_.includes(Memory.overlord.registered_spawns, id);
 };
 
-Overlord.prototype.register_spawn = function(spawn, id) {
+Overlord.register_spawn = function(spawn, id) {
 	if (this.is_spawn_registered(id)) {
 		console.log("Registering Spawn: " + id);
 		Memory.overlord.registered_spawns.push(id);
 	}
 };
 
-Overlord.prototype.register_creep = function(creep, id) {
+Overlord.register_creep = function(creep, id) {
 };
 
-Overlord.prototype.register_flag = function(flag, flag_name) {
+Overlord.register_flag = function(flag, flag_name) {
 };
 
-Overlord.prototype.register_structure = function(structure, id) {
+Overlord.register_structure = function(structure, id) {
 	switch(structure.structureType) {
 		case STRUCTURE_SPAWN:
 			this.register_spawn(structure, id);
